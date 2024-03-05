4ba835500b0ebd1164bc65dc72eef1fbb8a951d8
TomasAssmbly <tomas.santos@assembly.pt>
2024-03-05 19:40:02 +0000

    .

---
+++ PaymentDetails/Models/PaymentDetail.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PaymentDetails.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int PaymentDetailId { get; set; }
+        //[Column("card_owner_name", TypeName = "nvarchar(100)")]
+        [Column(TypeName ="nvarchar(100)")]
+        public string CardOwnerName { get; set; } = "";
+        [Column(TypeName = "nvarchar(16)")]
+        public string CardNumber { get; set; } = "";
+        [Column(TypeName = "nvarchar(5)")]
+        public string ExpirationDate { get; set; } = "";
+        [Column(TypeName = "nvarchar(3)")]
+        public string SecurityCode { get; set; } = "";
+
+    }
+}
---
+++ PaymentDetails/Models/PaymentDetailContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PaymentDetails.Models
+{
+    public class PaymentDetailContext : DbContext
+    {
+
+
+        public PaymentDetailContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+    }
+}
